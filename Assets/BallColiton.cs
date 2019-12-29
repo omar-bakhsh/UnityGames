e5a15fe5e4edaedf6d6e6d7d37840b755033f27b
Omar bakhsh <40505899+omar-bakhsh@users.noreply.github.com>
2019-12-29 04:58:53 +0300

    all file

---
+++ Assets/BallColiton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+public class BallColiton : MonoBehaviour{
+public BallControll ctrl ;
+public NumScript NumCTR;
+public num2S NumCTR2;
+public NumScript NumCTR3;
+public LastLineS Winner;
+int y =0;
+ void OnCollisionEnter(Collision col){
+
+
+ 	/*if(col.collider.tag=="num")
+ 	{
+			y++;													
+	  NumCTR.transform.position =new Vector3(25, 10, 10);
+
+
+	}*/
+	
+	switch (col.collider.tag) {
+
+    case "num":
+
+	  NumCTR.transform.position =new Vector3(25, 10, 10);
+	   y++;	
+     break;
+
+      case "num2": // statement sequence
+      NumCTR2.transform.position =new Vector3(25, 10, 10);
+     break;
+
+
+      case "num3": // statement sequence
+      NumCTR3.transform.position =new Vector3(25, 10, 10);
+     break;
+
+
+// default statement sequence
+}
+  if(y>=3){
+			Winner.GetComponent<Rigidbody>().mass=1;
+			Winner.transform.position = new Vector3(-25, 30, 1027);
+			Debug.Log("tru");
+
+	}
+
+	/*OnCollisionEnter End */}
+
+
+}
---
+++ Assets/BallControll.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControll : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float stret =4500; 
+	public float back =1500;
+	public float left =2225;
+	public float right =2225;
+	public float jump =600;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	// streert fun
+    	if (Input.GetKey("w")){
+    	rb.AddForce(0,0,stret * Time.deltaTime);}
+    	// back fun 
+    	if (Input.GetKey("s")){
+    	rb.AddForce(0,0,-back * Time.deltaTime);}
+		// left fun most have negetive value to go left in - X POSiton
+    	if(Input.GetKey("a")){
+    	rb.AddForce(-left* Time.deltaTime,0,0 );}
+    	// Right fun
+    	if(Input.GetKey("d")){
+    	rb.AddForce(right* Time.deltaTime,0,0 );}
+    	// jump fun
+    	if (Input.GetKey("space")){
+    	rb.AddForce(0,jump * Time.deltaTime,0);}
+        
+    }
+    	
+}
